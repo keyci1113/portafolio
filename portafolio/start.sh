edbbff73b95afa36f2a3b45d508eb26ff998cf37
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-12-01 19:30:50 -0600

    test deploy #10

--- portafolio/start.sh
+++ portafolio/start.sh
@@ -1,6 +1,6 @@
 echo "Deploy..."
 mkdir ~/.ssh
-echo -e "$key"> ~/.ssh/key.pem
+echo "$key"> ~/.ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 chmod 400 ~/.ssh/key.pem
 ssh -i "~/.ssh/key.pem" $DEPLOY_USER@$DEPLOY_HOST "cd ~/portafolio/;sudo git checkout .;sudo git pull;sh portafolio/up.sh"
