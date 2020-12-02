31e597d7d24482d44c5cf884f1b0c5a7d8edf254
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-12-01 18:33:18 -0600

    test deploy

--- portafolio/up.sh
+++ portafolio/up.sh
@@ -1,2 +1,8 @@
-docker build --tag portafolio .
-docker run -d -p 80:80 portafolio
+cd ~/portafolio/portafolio
+sudo docker build --tag portafolio .
+containerID=$(sudo docker container ls | sudo grep "0.0.0.0:80" | sudo awk '{print $1}')
+echo $containerID
+sudo docker stop $containerID
+sudo docker rm $containerID
+sudo docker run -d -p 80:80 portafolio
+sudo docker system prune -a --force
