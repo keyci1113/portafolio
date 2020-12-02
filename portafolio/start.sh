27fcaa9e7472db1daf60ac11c775d87ec6f2b710
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-12-01 19:29:11 -0600

    test deploy #9

--- portafolio/start.sh
+++ portafolio/start.sh
@@ -3,6 +3,6 @@ mkdir ~/.ssh
 echo -e "$key"> ~/.ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 chmod 400 ~/.ssh/key.pem
-ssh -i "~/.ssh/key.pem" -o StrictHostKeyChecking=yes  $DEPLOY_USER@$DEPLOY_HOST "cd ~/portafolio/;sudo git checkout .;sudo git pull;sh portafolio/up.sh"
+ssh -i "~/.ssh/key.pem" $DEPLOY_USER@$DEPLOY_HOST "cd ~/portafolio/;sudo git checkout .;sudo git pull;sh portafolio/up.sh"
 rm ~/.ssh/key.pem
 echo "Deploy success!!! you are awesome."
