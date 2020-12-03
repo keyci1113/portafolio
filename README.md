69b1b84a368f8e7d11c0b60aefca967132a5ae49
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-12-02 21:14:14 -0600

    change port

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Portafolio
 este es un repositorio con react js
 ## Hosting
-[portafolio](http://ec2-54-236-33-206.compute-1.amazonaws.com/)
+[portafolio](http://ec2-54-236-33-206.compute-1.amazonaws.com:443/)
 ## Iniciar el proyecto 
 en una terminal ejecute \
 `cd portafolio`\
--- portafolio/up.sh
+++ portafolio/up.sh
@@ -1,8 +1,8 @@
 cd ~/portafolio/portafolio
 sudo docker build --tag portafolio .
-containerID=$(sudo docker container ls | sudo grep "0.0.0.0:80" | sudo awk '{print $1}')
+containerID=$(sudo docker container ls | sudo grep "0.0.0.0:443" | sudo awk '{print $1}')
 echo $containerID
 sudo docker stop $containerID
 sudo docker rm $containerID
-sudo docker run -d -p 80:80 portafolio
+sudo docker run -d -p 443:80 portafolio
 sudo docker system prune -a --force
